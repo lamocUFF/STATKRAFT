1cbc73b9e95a1a86c990c8b1f455d463138dfb4a
Laboratório de modelagem Climática <lamoc.git@gmail.com>
2016-01-27 17:14:01 -0200

    xxx

--- adquire_gfs.sh
+++ adquire_gfs.sh
@@ -107,7 +107,7 @@ echo "gep20 0  t,y,x  ** chuva mm "$grads_data >>gfsens.ctl
 echo "endvars">>gfsens.ctl
 
 
-#grads -lbc "gfs.gs"  >>./LOG.prn 2>&1
+grads -lbc "gfs.gs"  >>./LOG.prn 2>&1
 grads -lbc "GFS_1P0.gs"  >>./LOG.prn 2>&1
 #grads -lbc "gfsens.gs"  >>./LOG.prn 2>&1
 
@@ -237,32 +237,32 @@ grads -lbc "figuras_gfs.gs"  >>./LOG.prn 2>&1
 #  as bacias estao cadastradas em CADASTRO/CADASTRADAS
 # ver documentacao para maiores detalhes
 #
-echo "*"                                                              >figuras_gfs.gs
-echo "* esse script é auto gerado. documentação em adquire_eta.sh"   >>figuras_gfs.gs
-echo "*By reginaldo.venturadesa@gmail.com "                             >>figuras_gfs.gs
-echo "'open gfs.ctl'"            >>figuras_gfs.gs
-#echo "*'set mpdset hires'"               >>figuras_gfs.gs
-echo "'set gxout shaded'"               >>figuras_gfs.gs
+echo "*"                                                              >figuras_gfs1P0.gs.gs
+echo "* esse script é auto gerado. documentação em adquire_eta.sh"   >>figuras_gfs1P0.gs.gs
+echo "*By reginaldo.venturadesa@gmail.com "                             >>figuras_gfs1P0.gs.gs
+echo "'open gfs.ctl'"            >>figuras_gfs1P0.gs.gs
+#echo "*'set mpdset hires'"               >>figuras_gfs1P0.gs.gs
+echo "'set gxout shaded'"               >>figuras_gfs1P0.gs.gs
 #
 # pega parametros de execucao do grads
 # se é retrato ou paisagem
 #
-echo "'q gxinfo'"   >>figuras_gfs.gs
-echo "var=sublin(result,2)"  >>figuras_gfs.gs
-echo "page=subwrd(var,4)" >>figuras_gfs.gs
-echo "*say page" >>figuras_gfs.gs
+echo "'q gxinfo'"   >>figuras_gfs1P0.gs.gs
+echo "var=sublin(result,2)"  >>figuras_gfs1P0.gs.gs
+echo "page=subwrd(var,4)" >>figuras_gfs1P0.gs.gs
+echo "*say page" >>figuras_gfs1P0.gs.gs
 #
 # se for retrato cria vpage
 #
-# echo "if (page ="8.5") " >>figuras_gfs.gs
-# echo "'set parea 0.5 8.5 1.5 10.2'" >>figuras_gfs.gs
-# echo "endif"                                  >>figuras_gfs.gs
+# echo "if (page ="8.5") " >>figuras_gfs1P0.gs.gs
+# echo "'set parea 0.5 8.5 1.5 10.2'" >>figuras_gfs1P0.gs.gs
+# echo "endif"                                  >>figuras_gfs1P0.gs.gs
 #
 #  data RODADA
 #
-echo "'set t  0'"                     >>figuras_gfs.gs
-echo "'q time'"                         >>figuras_gfs.gs
-echo "var0=subwrd(result,3)"            >>figuras_gfs.gs
+echo "'set t  0'"                     >>figuras_gfs1P0.gs.gs
+echo "'q time'"                         >>figuras_gfs1P0.gs.gs
+echo "var0=subwrd(result,3)"            >>figuras_gfs1P0.gs.gs
 
 
 
@@ -277,58 +277,58 @@ echo "'set rgb 99 251 94 107'">>coresdiaria.gs
 echo "'set clevs    05 10 15 20 25 30 35  50  70  100  150'">>coresdiaria.gs
 echo "'set ccols 00 44 45 47 49 34 37 39  22  23  27    29   99'  ">>coresdiaria.gs
 
-echo "'set lon -80.0000   -30.0000   '"                     >>figuras_gfs.gs
-echo "'set lat   -35 06.0000         ' "                                    >>figuras_gfs.gs
-echo "'set t 1 last'"                     >>figuras_gfs.gs
-echo "'q time'"                         >>figuras_gfs.gs
-echo "var1=subwrd(result,3)"            >>figuras_gfs.gs
-echo "ano1=substr(var1,9,4)"                       >>figuras_gfs.gs
-echo "mes1=substr(var1,6,3)"                       >>figuras_gfs.gs
-echo "dia1=substr(var1,4,2)"                       >>figuras_gfs.gs
+echo "'set lon -80.0000   -30.0000   '"                     >>figuras_gfs1P0.gs.gs
+echo "'set lat   -35 06.0000         ' "                                    >>figuras_gfs1P0.gs.gs
+echo "'set t 1 last'"                     >>figuras_gfs1P0.gs.gs
+echo "'q time'"                         >>figuras_gfs1P0.gs.gs
+echo "var1=subwrd(result,3)"            >>figuras_gfs1P0.gs.gs
+echo "ano1=substr(var1,9,4)"                       >>figuras_gfs1P0.gs.gs
+echo "mes1=substr(var1,6,3)"                       >>figuras_gfs1P0.gs.gs
+echo "dia1=substr(var1,4,2)"                       >>figuras_gfs1P0.gs.gs
 
-echo " t=1"                                    >>figuras_gfs.gs
-echo " while (t<=81)"                                    >>figuras_gfs.gs
-echo "'c'"   >>figuras_gfs.gs
+echo " t=1"                                    >>figuras_gfs1P0.gs.gs
+echo " while (t<=81)"                                    >>figuras_gfs1P0.gs.gs
+echo "'c'"   >>figuras_gfs1P0.gs.gs
 # data inicial previsao 
 
 
 
 
-echo "'set t 't"                                    >>figuras_gfs.gs
-echo "'q time'"                                    >>figuras_gfs.gs
-echo "datah=subwrd(result,3) "                                    >>figuras_gfs.gs
+echo "'set t 't"                                    >>figuras_gfs1P0.gs.gs
+echo "'q time'"                                    >>figuras_gfs1P0.gs.gs
+echo "datah=subwrd(result,3) "                                    >>figuras_gfs1P0.gs.gs
 # data 7 dias
-echo "ano6=substr(datah,9,4)"                       >>figuras_gfs.gs
-echo "mes6=substr(datah,6,3)"                       >>figuras_gfs.gs
-echo "dia6=substr(datah,4,2)"                       >>figuras_gfs.gs
+echo "ano6=substr(datah,9,4)"                       >>figuras_gfs1P0.gs.gs
+echo "mes6=substr(datah,6,3)"                       >>figuras_gfs1P0.gs.gs
+echo "dia6=substr(datah,4,2)"                       >>figuras_gfs1P0.gs.gs
 
 
 
 
-echo "'coresdiaria.gs'"                                         >>figuras_gfs.gs
-echo "'set gxout shaded'"                                    >>figuras_gfs.gs
-echo "'d sum(chuva,t='t',t='t+7')'"                                 >>figuras_gfs.gs
-echo "'draw string 2.5 8.3 PRECIPITACAO DIARIA GFS '"  >>figuras_gfs.gs
-echo "'draw string 2.5 8.1 RODADA :'dia1'/'mes1'/'ano1"               >>figuras_gfs.gs
-echo "'draw string 2.5 7.9 DIA    :'dia6'/'mes6'/'ano6"      >>figuras_gfs.gs
-#echo "'draw string 2.5 8.1 RODADA :"$DATA0" - "$hora"Z'"     >>figuras_gfs.gs
-#echo "'draw string 2.5 7.9 Periodo:'datah"    >>figuras_gfs.gs
-echo "'set rgb 50   255   255    255'" >>figuras_gfs.gs
-echo "'basemap.gs O 50 0 M'" >>figuras_gfs.gs
-echo "'set mpdset hires'" >>figuras_gfs.gs
-echo "'set map 15 1 6'" >>figuras_gfs.gs
-echo "'draw map'" >>figuras_gfs.gs     
-echo "'cbarn.gs'"                                            >>figuras_gfs.gs
-echo "'draw shp ../../CONTORNOS/SHAPES/BRASIL.shp'"     >>figuras_gfs.gs
-echo "'plota.gs'"                             >>figuras_gfs.gs
-#echo  "plotausina(bacia,page)"                          >>figuras_gfs.gs  
-echo "'plota_hidrografia.gs'"                          >>figuras_gfs.gs
-echo "'printim prec_diaria_'datah'.0P25.png white'"      >>figuras_gfs.gs
-echo "t=t+8"                                    >>figuras_gfs.gs
-echo "endwhile"                                    >>figuras_gfs.gs
-echo "'quit'" >>figuras_gfs.gs 
+echo "'coresdiaria.gs'"                                         >>figuras_gfs1P0.gs.gs
+echo "'set gxout shaded'"                                    >>figuras_gfs1P0.gs.gs
+echo "'d sum(chuva,t='t',t='t+7')'"                                 >>figuras_gfs1P0.gs.gs
+echo "'draw string 2.5 8.3 PRECIPITACAO DIARIA GFS '"  >>figuras_gfs1P0.gs.gs
+echo "'draw string 2.5 8.1 RODADA :'dia1'/'mes1'/'ano1"               >>figuras_gfs1P0.gs.gs
+echo "'draw string 2.5 7.9 DIA    :'dia6'/'mes6'/'ano6"      >>figuras_gfs1P0.gs.gs
+#echo "'draw string 2.5 8.1 RODADA :"$DATA0" - "$hora"Z'"     >>figuras_gfs1P0.gs.gs
+#echo "'draw string 2.5 7.9 Periodo:'datah"    >>figuras_gfs1P0.gs.gs
+echo "'set rgb 50   255   255    255'" >>figuras_gfs1P0.gs.gs
+echo "'basemap.gs O 50 0 M'" >>figuras_gfs1P0.gs.gs
+echo "'set mpdset hires'" >>figuras_gfs1P0.gs.gs
+echo "'set map 15 1 6'" >>figuras_gfs1P0.gs.gs
+echo "'draw map'" >>figuras_gfs1P0.gs.gs     
+echo "'cbarn.gs'"                                            >>figuras_gfs1P0.gs.gs
+echo "'draw shp ../../CONTORNOS/SHAPES/BRASIL.shp'"     >>figuras_gfs1P0.gs.gs
+echo "'plota.gs'"                             >>figuras_gfs1P0.gs.gs
+#echo  "plotausina(bacia,page)"                          >>figuras_gfs1P0.gs.gs  
+echo "'plota_hidrografia.gs'"                          >>figuras_gfs1P0.gs.gs
+echo "'printim prec_diaria_'datah'.0P25.png white'"      >>figuras_gfs1P0.gs.gs
+echo "t=t+8"                                    >>figuras_gfs1P0.gs.gs
+echo "endwhile"                                    >>figuras_gfs1P0.gs.gs
+echo "'quit'" >>figuras_gfs1P0.gs.gs 
 
-grads -lbc "figuras_gfs.gs"  >>./LOG.prn 2>&1
+grads -lbc "figuras_gfs1P0.gs.gs"  >>./LOG.prn 2>&1
 cd ..
 cd ..
 
