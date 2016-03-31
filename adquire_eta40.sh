b07448faddaa07a6dcdff49339db30f0d92725ca
Laboratório de modelagem Climática <lamoc.git@gmail.com>
2016-03-31 16:24:30 -0300

    xxx

--- adquire_eta40.sh
+++ adquire_eta40.sh
@@ -332,8 +332,7 @@ echo "'plota_hidrografia.gs'"     >>figura3.gs
 echo "plotausina(bacia,page)" >>figura3.gs    
 echo "'cbarn.gs 1.0 0 '" >>figura3.gs
 echo "'printim 'bacia'_semanaoperativa_2_"$data".png white'"                       >>figura3.gs
-#echo "jj=write('lista_de_figuras.dat',bacia'_semanaoperativa_2_"$data".png' " >>figura3.gs
-#
+echo "jj=write('lista_de_figuras.dat',bacia'_semanaoperativa_2_$data.png') " >>figura3.gs#
 # FIGURA RETRATO SEMANA 7 DIAS CORRIDOS 
 #
 echo "'reset'"                        >>figura3.gs
@@ -363,7 +362,7 @@ echo "'plota_hidrografia.gs'"     >>figura3.gs
 echo "plotausina(bacia,page)" >>figura3.gs  
 echo "'cbarn.gs 1.0 0 '" >>figura3.gs
 echo "'printim 'bacia'_prec07dias_"$data"_"$hora"Z.png white'"       >>figura3.gs
-#echo "jj=write('lista_de_figuras.dat',bacia'_prec07dias_"$data"_"$hora"Z.png' " >>figura3.gs
+echo "jj=write('lista_de_figuras.dat',bacia'_prec07dias_$data_$horaZ.png') " >>figura3.gs
 echo "*say t0"                           >>figura3.gs
 #
 #
@@ -398,7 +397,7 @@ echo "'plota_hidrografia.gs'"     >>figura3.gs
 echo "plotausina(bacia,page)" >>figura3.gs  
 echo "'cbarn.gs 1.0 0 '" >>figura3.gs
 echo "'printim 'bacia'_diaria_'var1'.png white'"                       >>figura3.gs
-#echo "jj=write('lista_de_figuras.dat',bacia'_diaria_'var1'.png' " >>figura3.gs
+echo "jj=write('lista_de_figuras.dat',bacia'_diaria_'var1'.png') " >>figura3.gs
 
 echo "'c'"                                                             >>figura3.gs
 echo "t=t+1"                    >>figura3.gs
